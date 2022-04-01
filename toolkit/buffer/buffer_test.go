765db303d75b62514a137462cdd50a6863f91b76
Ivanqi <512350225@qq.com>
2022-04-01 18:25:36 +0800

    【开发】buffer 测试案例

--- toolkit/buffer/buffer_test.go
+++ toolkit/buffer/buffer_test.go
@@ -2,7 +2,11 @@ package buffer
 
 import (
 	"fmt"
+	"math/rand"
+	"sync"
+	"sync/atomic"
 	"testing"
+	"time"
 )
 
 func TestBufferNew(t *testing.T) {
@@ -173,4 +177,167 @@ func TestBufferGet(t *testing.T) {
 	if err == nil {
 		t.Fatal("它仍然可以从封闭的缓冲区中获取数据!")
 	}
+}
+
+func TestBufferPutAndGetInParallel(t *testing.T) {
+	bufferSize := uint32(50)
+	buf, err := NewBuffer(bufferSize)
+	if err != nil {
+		t.Fatalf("新建缓冲区时出错：%s (大小：%d)", err, bufferSize)
+	}
+
+	maxPuttingNumber := bufferSize + uint32(rand.Int31n(20))
+	maxGettingNumber := bufferSize + uint32(rand.Int31n(20))
+
+	puttingCount := maxPuttingNumber
+	gettingCount := maxGettingNumber
+
+	marks := make([]uint8, maxPuttingNumber)
+	var lock sync.Mutex
+
+	t.Run("All in parallel", func(t *testing.T) {
+		t.Run("Put1", func(t *testing.T) {
+			t.Parallel()
+			begin := uint32(0)
+			end := maxPuttingNumber / 2
+			for i := begin; i < end; i++ {
+				ok, err := buf.Put(i)
+				if err != nil {
+					t.Fatalf("将数据放入缓冲区出错: %s (数据: %d)", err, i)
+				}
+
+				if !ok && atomic.LoadUint32(&gettingCount) == 0 && buf.Len() < buf.Cap() {
+					t.Fatalf("无法将数据放入缓冲区! (数据: %d)", i)
+				}
+
+				atomic.AddUint32(&puttingCount, ^uint32(0))
+			}
+		})
+
+		t.Run("Put2", func(t *testing.T) {
+			t.Parallel()
+			begin := maxGettingNumber / 2
+			end := maxPuttingNumber
+			for i := begin; i < end; i++ {
+				ok, err := buf.Put(i)
+				if err != nil {
+					t.Fatalf("将数据放入缓冲区出错: %s, (数据: %d)", err, i)
+				}
+
+				if !ok && atomic.LoadUint32(&gettingCount) == 0 && buf.Len() < buf.Cap() {
+					t.Fatalf("无法将数据放入缓冲区! (数据: %d)", i)
+				}
+
+				atomic.AddUint32(&puttingCount, ^uint32(0))
+			}
+		})
+
+		t.Run("Get1", func(t *testing.T) {
+			t.Parallel()
+			max := bufferSize/2 + 1
+			for i := uint32(0); i < max; i++ {
+				d, err := buf.Get()
+				if err != nil {
+					t.Fatalf("从缓冲区获取数据出错: %s", err)
+				}
+
+				if d == nil && atomic.LoadUint32(&puttingCount) == 0 && buf.Len() != 0 {
+					t.Fatalf("得到一个空的数据! (len: %d)", buf.Len())
+				}
+
+				atomic.AddUint32(&gettingCount, ^uint32(0))
+				if d != nil {
+					datum := d.(uint32)
+					lock.Lock()
+					marks[int(datum)]++
+					lock.Unlock()
+				}
+			}
+		})
+
+		t.Run("Get2", func(t *testing.T) {
+			t.Parallel()
+			max := bufferSize/2 + 2
+			for i := uint32(0); i < max; i++ {
+				d, err := buf.Get()
+				if err != nil {
+					t.Fatalf("从缓冲区获取数据出错: %s", err)
+				}
+
+				if d == nil && atomic.LoadUint32(&puttingCount) == 0 && buf.Len() != 0 {
+					t.Fatalf("获取空的数据! (len: %d)", buf.Len())
+				}
+
+				atomic.AddUint32(&gettingCount, ^uint32(0))
+				if d != nil {
+					datum := d.(uint32)
+					lock.Lock()
+					marks[int(datum)]++
+					lock.Unlock()
+				}
+			}
+		})
+	})
+
+	for i, m := range marks {
+		if m > 1 {
+			t.Fatalf("多次获取数据: %d", i)
+		}
+	}
+}
+
+func TestBufferCloseInParallel(t *testing.T) {
+	bufferSize := uint32(100)
+	buf, err := NewBuffer(bufferSize)
+	if err != nil {
+		t.Fatalf("新建缓冲区时出错：%s (大小：%d)", err, bufferSize)
+	}
+	maxNumber := bufferSize + uint32(rand.Int31n(100))
+	t.Run("Put", func(t *testing.T) {
+		t.Parallel()
+		for i := uint32(0); i < maxNumber; i++ {
+			_, err := buf.Put(i)
+			if err != nil && !buf.Closed() {
+				t.Fatalf("将数据放入缓冲区出错: %s, (数据: %d)", err, i)
+			}
+			if err == nil && buf.Closed() {
+				t.Fatalf("仍然可以将数据放入封闭缓冲区! (数据: %d)", i)
+			}
+		}
+	})
+	t.Run("Get", func(t *testing.T) {
+		t.Parallel()
+		max := bufferSize/2 + 1
+		for i := uint32(0); i < max; i++ {
+			_, err := buf.Get()
+			if err != nil && !buf.Closed() {
+				t.Fatalf("从缓冲区获取数据时错误: %s, (数据: %d)", err, i)
+			}
+			if buf.Closed() {
+				if _, err = buf.Get(); err == nil {
+					t.Fatalf("仍然可以将数据放入封闭缓冲区! (数据: %d)", i)
+				}
+			}
+		}
+	})
+	t.Run("Close", func(t *testing.T) {
+		t.Parallel()
+		time.Sleep(time.Millisecond)
+		ok := buf.Close()
+		if !ok {
+			t.Fatal("不能关闭缓冲区")
+		}
+
+		if !buf.Closed() {
+			t.Fatalf("缓冲区状态不一致: 预期关闭: %v, 实际关闭: %v", true, buf.Closed())
+		}
+
+		ok = buf.Close()
+		if ok {
+			t.Fatal("仍然不能关闭缓冲区！")
+		}
+		if !buf.Closed() {
+			t.Fatalf("缓冲区状态不一致: 预期关闭: %v, 实际关闭: %v", true, buf.Closed())
+		}
+	})
 }
