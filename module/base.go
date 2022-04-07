7c9c5b13889a9e8d08c549b2cef585cd5957fb28
Ivanqi <512350225@qq.com>
2022-04-08 00:54:39 +0800

    【开发】新增功能

--- module/base.go
+++ module/base.go
@@ -1,5 +1,7 @@
 package module
 
+import "net/http"
+
 // Counts 代表用于汇集组件内部计数的类型
 type Counts struct {
 	// CalledCount 代表调用计数
@@ -22,6 +24,9 @@ type SummaryStruct struct {
 	Extra     interface{} `json:"extra,omitempty"`
 }
 
+// ParseResponse 代笔用于解析HTTP响应的函数的类型
+type ParseResponse func(httpResp *http.Response, respDepth uint32) ([]Data, []error)
+
 // Module 代表组件的基础接口
 // 该接口的实现类型必须是并发安全
 type Module interface {
@@ -31,9 +36,59 @@ type Module interface {
 	Addr() string
 	// Score 用于获取当前组件的评分
 	Score() uint64
+	// 用于设置当前组件的评分
+	SetScore(score uint64)
 	// ScoreCalculator 用于获取评分计算器
-	// ScoreCalculator() CalculateScore
+	ScoreCalculator() CalculateScore
 	// CallCount 用于获取当前组件被调用的计数
 	CalledCount() uint64
-	// AcceptedCount
+	// AcceptedCount 用于获取被当前组件接受的调用计数
+	// 组件一般会由于超负荷或参数有误而拒绝调用
+	AcceptedCount() uint64
+	// CompletedCount 用于获取当前组件已成功完成的调用的计数
+	CompletedCount() uint64
+	// HandlingNumber 用于获取当前组件正在处理的调用数量
+	HandlingNumber() uint64
+	// Counts 用于一次性获取所有计数
+	Counts() Counts
+	// Summary 用于获取组件的摘要
+	Summary() SummaryStruct
+}
+
+// Downloader 代表下载器的接口类型
+// 该接口实现类型必须是并发安全的
+type Downloader interface {
+	Module
+	// Download 会根据请求获取内容并返回响应
+	Download(reg *Request) (*Response, error)
+}
+
+// Analyzer 代表分析器的接口类型
+// 该接口的实现类型必须是并发安全
+type Analyzer interface {
+	Module
+	// RespParsers 会返回当前分析器使用的响应解析函数的列表
+	RespParsers() []ParseResponse
+	// Analyze 会根据规则分析响应并返回请求和条目
+	// 响应需要分别经过若干响应解析函数的处理，然后合并结果
+	Analyze(resp *Response) ([]Data, []error)
+}
+
+// ProcessItem 代表用于处理条目的函数的类型
+type ProcessItem func(item Item) (result Item, err error)
+
+// Pipeline 代表条目处理管道的接口类型
+// 该接口的实现类型必须是并发安全
+type Pipeline interface {
+	Module
+	// ItemProcessors 会返回当前条目处理管道使用的条目处理函数的列表
+	ItemProcessors() []ProcessItem
+	// Send 会向条目处理管道发送条目
+	// 条目需要依次经过若干条目处理函数的处理
+	Send(item Item) []error
+	// FailFast方法会返回一个布尔值。该值表示当前条目处理管道是否是快速失败的
+	// 这里的快速失败是指：只要在处理某个条目时在某一个步骤上出错，那么条目处理管道就会忽略掉后续的所有处理步骤并报告错误
+	FailFast() bool
+	// 设置是否快速失败
+	SetFailFast(failFast bool)
 }
--- module/score.go
+++ module/score.go
@@ -4,3 +4,22 @@ package module
 type CalculateScore func(count Counts) uint64
 
 // CalculateScoreSimple 代笔简易的组件评分计算函数
+func CalculateScoreSimple(counts Counts) uint64 {
+	return counts.CalledCount + counts.AcceptedCount<<1 + counts.CompletedCount<<2 + counts.HandlingNumber<<4
+}
+
+// SetScore 用于设置给定组件的评分
+func SetScore(module Module) bool {
+	calculator := module.ScoreCalculator()
+	if calculator == nil {
+		calculator = CalculateScoreSimple
+	}
+
+	newScore := calculator(module.Counts())
+	if newScore == module.Score() {
+		return false
+	}
+
+	module.SetScore(newScore)
+	return true
+}
