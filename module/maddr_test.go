f2b7f9bb2b670d1014524e3332100fe703b1ee34
Ivanqi <512350225@qq.com>
2022-04-09 22:37:44 +0800

    【开发】增加新功能

---
+++ module/maddr_test.go
@@ -0,0 +1,57 @@
+package module
+
+import (
+	"strconv"
+	"testing"
+)
+
+var legalNetworkMap = map[string]struct{}{
+	"http":  struct{}{},
+	"https": struct{}{},
+}
+
+var legalIPMap = map[string]struct{}{
+	"192.0.2.1":    struct{}{},
+	"2001:db8::68": struct{}{},
+}
+
+func TestAddrNew(t *testing.T) {
+	var network string
+	var ip string
+	port := uint64(8080)
+
+	for network = range legalNetworkMap {
+		for ip = range legalIPMap {
+			addr, err := NewAddr(network, ip, port)
+			if err != nil {
+				t.Fatalf("创建地址时出错: %s (network: %s, ip: %s, port: %d)", err, network, ip, port)
+			}
+
+			if addr == nil {
+				t.Fatal("不能创建地址!")
+			}
+
+			if addr.Network() != network {
+				t.Fatalf("地址的网络不一致：预期:%s, 实际: %s", network, addr.Network())
+			}
+
+			expectedIPPort := ip + ":" + strconv.FormatUint(port, 10)
+			if addr.String() != expectedIPPort {
+				t.Fatalf("地址的IP/端口不一致： 预期: %s, 实际: %s", expectedIPPort, addr.String())
+			}
+		}
+	}
+
+	network = "http"
+	_, err := NewAddr(network, ip, port)
+	if err == nil {
+		t.Fatalf("使用非法网络 %q! 创建缓冲区时没有错误!", network)
+	}
+
+	network = "http"
+	ip = "127.0.0.1"
+	_, err = NewAddr(network, ip, port)
+	if err == nil {
+		t.Fatalf("使用非法网络 %q! 创建缓冲区时没有错误！", network)
+	}
+}
--- module/mid_test.go
+++ module/mid_test.go
@@ -1,9 +1,66 @@
 package module
 
+import (
+	"net"
+	"testing"
+)
+
 var legalMIDs = []MID{}
 
 func init() {
 	for _, mt := range legalTypes {
+		for mip := range legalIPMap {
+			addr, _ := NewAddr("http", mip, 8080)
+			mid, _ := GenMID(mt, DefaultSNGen.Get(), addr)
+			legalMIDs = append(legalMIDs, mid)
+		}
+	}
+}
 
+var illegalMIDs = []MID{
+	MID("D"),
+	MID("DZ"),
+	MID("D1|"),
+	MID("D1|127.0.0.1:-1"),
+	MID("D1|127.0.0.1:"),
+	MID("D1|127.0.0.1"),
+	MID("D1|127.0.0."),
+	MID("D1|127"),
+	MID("D1|127.0.0.0.1:8080"),
+	MID("DZ|127.0.0.1:8080"),
+	MID("A"),
+	MID("AZ"),
+	MID("A1|"),
+	MID("A1|127.0.0.1:-1"),
+	MID("A1|127.0.0.1:"),
+	MID("A1|127.0.0.1"),
+	MID("A1|127.0.0."),
+	MID("A1|127"),
+	MID("A1|127.0.0.0.1:8080"),
+	MID("AZ|127.0.0.1:8080"),
+	MID("P"),
+	MID("PZ"),
+	MID("P1|"),
+	MID("P1|127.0.0.1:-1"),
+	MID("P1|127.0.0.1:"),
+	MID("P1|127.0.0.1"),
+	MID("P1|127.0.0."),
+	MID("P1|127"),
+	MID("P1|127.0.0.0.1:8080"),
+	MID("PZ|127.0.0.1:8080"),
+	MID("M1|127.0.0.1:8080"),
+}
+
+func TestMIDGenAndSplit(t *testing.T) {
+	addr, _ := NewAddr("http", "127.0.0.1", 8080)
+	addrs := []net.Addr{nil, addr}
+	for _, addr := range addrs {
+		for _, mt := range legalTypes {
+			expectedSN := DefaultSNGen.Get()
+			mid, err := GenMID(mt, expectedSN, addr)
+			if err != nil {
+				t.Fatalf("生成模块ID时出错: %s (类型: %s, sn: %d)")
+			}
+		}
 	}
 }
