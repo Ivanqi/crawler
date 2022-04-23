5efd59c083a1ceb34cfd22cb51fee569133c0776
Ivanqi <512350225@qq.com>
2022-04-23 23:38:52 +0800

    【开发】分析器增加新代码

--- module/local/analyzer/analyzer_test.go
+++ module/local/analyzer/analyzer_test.go
@@ -111,3 +111,108 @@ func TestNew(t *testing.T) {
 		}
 	}
 }
+
+func TestAnalyze(t *testing.T) {
+	number := uint32(10)
+	method := "GET"
+	expectedURL := "https://gitee.com/Ivanmax/algorithm"
+	expectedDepth := uint32(1)
+	resps := getTestingResps(number, method, expectedURL, expectedDepth, t)
+	mid := module.MID("D1|127.0.0.1:8000")
+	parsers := []module.ParseResponse{genTestingRespParser(false)}
+	a, err := New(mid, parsers, nil)
+	if err != nil {
+		t.Fatalf("创建分析器时出错: %s (mid: %s)", err, mid)
+	}
+
+	data := []module.Data{}
+	parseErrors := []error{}
+	for _, resp := range resps {
+		data1, parseErrors1 := a.Analyze(resp)
+		data = append(data, data1...)
+		parseErrors = append(parseErrors, parseErrors1...)
+	}
+
+	for i, e := range parseErrors {
+		t.Errorf("解析响应时出错: %s(索引: %d)", e, i)
+	}
+
+	var count int
+	for i, d := range data {
+		if d == nil {
+			t.Fatalf("nil datum! (index: %d)", i)
+		}
+
+		if _, ok := d.(*module.Request); ok {
+			continue
+		}
+
+		item, ok := d.(module.Item)
+		if !ok {
+			t.Errorf("不一致的索引类型: 预期: %T, 实际: %T (index: %d)", int(0), item["index"], i)
+		}
+
+		if item["url"] != expectedURL {
+			t.Errorf("不一致的 URL: 预期: %s, 实际: %s (index: %d)", expectedURL, item["url"], i)
+		}
+
+		index, ok := item["index"].(int)
+		if !ok {
+			t.Errorf("不一致的索引类型。预期: %T, 实际: %T(index: %d)", int(0), item["index"], 1)
+		}
+
+		if index != count {
+			t.Errorf("不一致的索引。预期: %d, 实际: %d(索引: %d)", count, index, i)
+		}
+
+		depth, ok := item["depth"].(uint32)
+		if !ok {
+			t.Errorf("不一致的深度类型. 预期: %T, 实际: %T(index: %d)", uint32(0), item["depth"], i)
+		}
+
+		if depth != expectedDepth {
+			t.Errorf("深度不一致。预期: %d, 实际: %d(索引: %d)", expectedDepth, depth, i)
+		}
+
+		count++
+	}
+
+	// 测试参数有误的情况。
+	// 测试响应为nil的情况。
+	_, errs := a.Analyze(nil)
+	if len(errs) == 0 {
+		t.Fatal("下载无响应时没有错误！")
+	}
+
+	// 测试HTTP响应为nil的情况。
+	resp := module.NewResponse(nil, 0)
+	_, errs = a.Analyze(resp)
+	if len(errs) == 0 {
+		t.Fatalf("使用非法响应  %#v! 分析响应时没有错误", parsers)
+	}
+
+	// 测试HTTP请求为nil的情况。
+	httpResp := &http.Response{
+		Request: nil,
+		Body:    nil,
+	}
+
+	resp = module.NewResponse(httpResp, 0)
+	_, errs = a.Analyze(resp)
+	if len(errs) == 0 {
+		t.Fatalf("使用 nil 请求 URL 分析响应时没有错误！")
+	}
+
+	// 测试HTTP请求URL为nil的情况。
+	httpReq, _ := http.NewRequest(method, expectedURL, nil)
+	httpReq.URL = nil
+	httpResp = &http.Response{
+		Request: httpReq,
+		Body:    nil,
+	}
+	resp = module.NewResponse(httpResp, 0)
+	_, errs = a.Analyze(resp)
+	if len(errs) == 0 {
+		t.Fatalf("使用 nil 请求 URL 分析响应时没有错误")
+	}
+}
